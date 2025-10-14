34647f05a057804e3fc0716fe2b93ec517cf3282
Anshul Verma <av4862187@gmail.com>
2025-10-14 11:04:46 +0530

    dom

---
+++ JavaScript/ES6/Dom.js
@@ -0,0 +1,8 @@
+const div=document.createElement("div");
+const h1=document.createElement("h1");
+h1.innerText="Hello Abes College";
+
+div.appendChild(h1);
+
+const maindiv=document.getElementById("maindiv");
+maindiv.appendChild(div)
