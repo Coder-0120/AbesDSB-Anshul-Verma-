c7c0a20d041fcd63a138dbb91e5b55f46ced2ed2
Anshul Verma <av4862187@gmail.com>
2025-10-31 14:36:49 +0530

    code

---
+++ Frontend/Anshul/src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Card.css";
+const Card = () => {
+  return (
+    <div id='card'>
+        <img src='' height={100} width={100}></img>
+        <h1>Abesec</h1>
+        <h1>BTech</h1>
+    </div>
+  )
+}
+
+export default Card
