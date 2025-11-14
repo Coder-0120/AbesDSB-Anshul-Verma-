5aa4e9cca16ba5382d7bbe7357f33c0e08fb1bbc
Anshul Verma <av4862187@gmail.com>
2025-11-14 14:58:35 +0530

    update code

--- Frontend/Anshul/src/App.jsx
+++ Frontend/Anshul/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import Link1 from './Components/Link1'
+import React, { useEffect, useState } from 'react'
+import Fashion from './Components/Fashion'
 
 const App = () => {
+  const[books,setBooks]=useState([]);
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(data=>{
+      setBooks(data);
+    })
+  },[])
   return (
-    <div>
-      <Link1/>
-      
+    <div style={{display:'flex',flexDirection:'row',gap:'2px',flexWrap:'wrap'}}>
+      {
+        books.map((b,i)=>(
+          <Fashion key={i}  props={b}/>
+        ))
+
+      }
     </div>
   )
 }
