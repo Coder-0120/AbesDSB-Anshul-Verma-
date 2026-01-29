bf086754be762a81dd5a84c84c35b44928bd2895
Anshul Verma <av4862187@gmail.com>
2026-01-29 11:49:53 +0530

    http server

---
+++ 6th_Sem_FSD/29-01-2026/index.js
@@ -0,0 +1,10 @@
+const http=require('http');
+const Server=http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain');
+    res.end('Hello World\n');
+})
+
+Server.listen(3000, ()=>{
+    console.log('Server running at http://localhost:3000/');
+});
