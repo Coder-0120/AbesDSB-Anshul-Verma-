cce51e823a5fb0c38ebe225db789202c48448932
Anshul Verma <av4862187@gmail.com>
2026-02-19 10:58:05 +0530

    error handling

--- 6th_Sem_FSD/06-02-2026/index2.js
+++ 6th_Sem_FSD/06-02-2026/index2.js
@@ -26,17 +26,3 @@ async function readWithPromisify() {
 
 readWithPromisify();
 
-const fs=require('fs').promises;
-
-async function writeFileExample(){
-  try{
-    await fs.writeFile('myfile2.txt','Hello, this is a sample text file.','utf8');
-    const data={name:'Anshul',age:21,city:'Hapur'};
-    await fs.writeFile('data.json',JSON.stringify(data,null,2),'utf8');
-    console.log('Files created successfully.');
-  }catch(err){
-    console.error('Error writing files:',err);
-  }
-}
-
-writeFileExample();
