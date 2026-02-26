39a2e643435e19e918e7d9dc05b39cddd5b1af2b
Anshul Verma <av4862187@gmail.com>
2026-02-26 11:21:54 +0530

    rest api

--- 6th_Sem_FSD/26-02-2026/backend/index.js
+++ 6th_Sem_FSD/26-02-2026/backend/index.js
@@ -24,6 +24,18 @@ let books=[
 app.get("/api/books",(req,res)=>{
     res.send(books);
 })
+
+app.get("/api/books/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const book=books.find((b)=>b.id==id);
+    if(book){
+        res.send(book);
+    }
+    else{
+        res.status(404).json({message:"Book not found"});
+    }
+    
+})
 app.listen(port,()=>{   
     console.log(`Server is running on ${port} `);
 
